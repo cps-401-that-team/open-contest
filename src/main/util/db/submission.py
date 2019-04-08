2f68d205f93497b25dd753de074136c73bd42c7f
mjoha563 <civilaircadet@gmail.com>
2019-04-08 09:45:46 -0700

    Start Judge Review Queue

--- src/main/util/db/submission.py
+++ src/main/util/db/submission.py
@@ -25,6 +25,7 @@ class Submission:
             self.errors      = details["errors"]
             self.answers     = details["answers"]
             self.result      = details["result"]
+            self.submissionStatus = details["submissionStatus"]
         else:
             self.id          = None
             self.user        = None
@@ -39,6 +40,7 @@ class Submission:
             self.errors      = []
             self.answers     = []
             self.result      = []
+            self.submissionStatus = []
 
     def get(id: str):
         with lock.gen_rlock():
@@ -60,7 +62,8 @@ class Submission:
             "outputs":   self.outputs,
             "errors":    self.errors,
             "answers":   self.answers,
-            "result":    self.result
+            "result":    self.result,
+            "submissionStatus": self.submissionStatus
         }
 
     def save(self):
@@ -106,7 +109,8 @@ class Submission:
                 "outputs":   self.outputs[:self.problem.samples],
                 "errors":    self.errors[:self.problem.samples],
                 "answers":   self.answers[:self.problem.samples],
-                "result":    self.result
+                "result":    self.result,
+                "submissionStatus": self.submissionStatus
             }
 
     def forEach(callback: callable):
